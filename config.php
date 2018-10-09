58bae62ca7fa0dd28f1738955433b90dd710f736
Unknown <iabiola54@gmail.com>
2018-10-08 19:25:06 -0500

    login and register

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_NAME', '');
 
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+//$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Check connection
 if($link === false){
