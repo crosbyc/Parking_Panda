b3142bdd051c353b4c6347a10c7edc6ee2afbe07
helinatariku@gmail.com <helinatariku@gmail.com>
2018-11-11 21:20:42 -0600

    made login page pretty

--- login.php
+++ login.php
@@ -125,16 +125,71 @@ if (isset($_POST['username']) and isset($_POST['password'])){
       </div>
       <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         <h1 class="page-header text-center">Welcome to Parking Panda</h1>
-
+		</div>
         <div class="row ">
-          <div class="col-md-6">
-            <h4>If You Have an Account -<br> Please Log In:</h4>
-            <form action="login.php" method="Post">
-              User name: <br>
-              <input type="text" name="username" pattern="[^\/;,*<>=+]*" class="form-control"><br><br> Password: <br>
-              <input type="password" name="password" pattern="[^\/;,*<>=+]*" id="inputPassword" class="form-control"><br><br>
-              <input type="submit" name="submitLogIn" value="Log In">
-            </form>
+			
+		<div class="container" style="margin-top:40px">
+		<div class="row">
+			<div class="col-sm-6 col-md-4 col-md-offset-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<strong> Sign in to continue</strong>
+					</div>
+					<div class="panel-body">
+						<form role="form" action="login.php" method="POST">
+							<fieldset>
+								<div class="row">
+									<div class="center-block">
+										<img class="profile-img"
+											src="img/panda.png" alt="">
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-sm-12 col-md-10  col-md-offset-1 ">
+										<div class="form-group">
+											<div class="input-group">
+												<span class="input-group-addon">
+													<i class="glyphicon glyphicon-user"></i>
+												</span> 
+												<input class="form-control" placeholder="Username" name="username" type="text" autofocus>
+											</div>
+										</div>
+										<div class="form-group">
+											<div class="input-group">
+												<span class="input-group-addon">
+													<i class="glyphicon glyphicon-lock"></i>
+												</span>
+												<input class="form-control" placeholder="Password" name="password" type="password" value="">
+											</div>
+										</div>
+										<div class="form-group">
+											<input type="submit" name="submitLogIn" class="btn btn-lg btn-primary btn-block" value="Log in">
+										</div>
+									</div>
+								</div>
+							</fieldset>
+						</form>
+					</div>
+					<div class="panel-footer ">
+						Don't have an account! <a href="register.php" onClick=""> Sign Up Here </a>
+					</div>
+                </div>
+			</div>
+		</div>
+	</div>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
             <?php if(isset($fmsg)) : ?>
 	<div class="alert alert-danger">
 		<?=$fmsg?>
